5e768a096a95267a8183c2fbe73baf856b2871ab
sabiyatabassum <50179634+sabiyatabassum@users.noreply.github.com>
2020-06-06 16:57:13 +0530

    Update Text Detection.py

--- Text Detection.py
+++ Text Detection.py
@@ -6,6 +6,16 @@ import time
 
 pytesseract.pytesseract.tesseract_cmd = 'C:\\Program Files (x86)\\Tesseract-OCR\\tesseract.exe'
 
+#####################################################################
+ #Here we just  turning the image into RGB
+  #As in OPencv the images pixels are in order BGR
+  #As we are using pytesseract we need to convert the image into RGB
+  #thats why COLOR_BGR2RGB
+  
+  #The output will be just a image is shown.
+  ####################################################################
+  
+
 img = cv2.imread('Resources/Test image.PNG')
 img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
